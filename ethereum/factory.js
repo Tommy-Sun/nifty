9334185df6cfe823aebd54b11dfd538424b8ec6d
Thomas Kebschull <tommykebschull@gmail.com>
2021-08-26 12:51:55 -0700

    To update default contract address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -12,5 +12,5 @@ const instance = new web3.eth.Contract(
 export default instance;
 
 //Here we are taking the web3 that we just created in web3.js and creating an instance of it. 
-//At the same time we are telling web3 that a deployed copy of the 'ArtPieceFactory' exists, and passing in the address that the contract is located at.
+//At the same time we are telling web3 that a deployed copy of the 'ArtPieceFactory' exists, and passing in the address that the contract is located at. 0x4D0fa3Ee7d743e89e0a40d44df99e03168DaFAE9
 
